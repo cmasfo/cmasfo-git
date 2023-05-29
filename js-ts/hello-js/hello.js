0827bfa20d350841204b8494d212e6a5298e6f34
cmasfo <cmasfo@gmail.com>
2023-05-29 22:44:45 +0900

    js ts

---
+++ js-ts/hello-js/hello.js
@@ -0,0 +1,6 @@
+
+// install node.js to run this javascript
+// node hello.js
+
+var message = 'Hello, JavaScript.';
+console.log(message);
---
+++ js-ts/hello-ts/hello.js
@@ -0,0 +1,7 @@
+// convert from ts to js then run
+// you need tsc(typescript compiler) to convert
+// npm install -g typescript
+// tsc hello.ts
+// node hello.js
+var message = 'Hello, TypeScript.';
+console.log(message);
